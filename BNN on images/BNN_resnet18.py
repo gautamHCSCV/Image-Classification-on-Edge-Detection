1ce8d71c94936b26aef8e762dc5d3b8460005f97
GAUTAM KUMAR <65457437+gautamHCSCV@users.noreply.github.com>
2021-10-30 17:33:27 +0530

    Add files via upload

---
+++ BNN on images/BNN_resnet18.py
@@ -0,0 +1,150 @@
+import tensorflow as tf
+import warnings
+import tensorflow_datasets as tfds
+from tensorflow.keras.layers import Dense,Conv2D,Flatten, Input,MaxPool2D
+from tensorflow.keras.models import Model
+warnings.filterwarnings('ignore')
+
+
+class Binarize(tf.keras.constraints.Constraint):
+    def __init__(self, ref_value = 1):
+            self.ref_value = ref_value
+
+    def __call__(self, w):
+            return tf.math.sign(w)*self.ref_value
+
+    def get_config(self):
+            return {'ref_value': self.ref_value}
+    
+
+# mnist
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.AUTOTUNE)
+
+# mnist dataset
+
+from tensorflow.keras.layers import Dense, Conv2D,  MaxPool2D, Flatten, GlobalAveragePooling2D,  BatchNormalization, Layer, Add
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+
+class ResnetBlock(Model):
+    """
+    A standard resnet block.
+    """
+
+    def __init__(self, channels: int, down_sample=False):
+        """
+        channels: same as number of convolution kernels
+        """
+        super().__init__()
+
+        self.__channels = channels
+        self.__down_sample = down_sample
+        self.__strides = [2, 1] if down_sample else [1, 1]
+
+        KERNEL_SIZE = (3, 3)
+        # use He initialization, instead of Xavier (a.k.a 'glorot_uniform' in Keras), as suggested in [2]
+        INIT_SCHEME = "he_normal"
+
+        self.conv_1 = Conv2D(self.__channels, strides=self.__strides[0],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME, kernel_constraint=Binarize(), bias_constraint=Binarize())
+        self.bn_1 = BatchNormalization(beta_constraint=Binarize(), gamma_constraint=Binarize())
+        self.conv_2 = Conv2D(self.__channels, strides=self.__strides[1],
+                             kernel_size=KERNEL_SIZE, padding="same", kernel_initializer=INIT_SCHEME, kernel_constraint=Binarize(), bias_constraint=Binarize())
+        self.bn_2 = BatchNormalization(beta_constraint=Binarize(), gamma_constraint=Binarize())
+        self.merge = Add()
+
+        if self.__down_sample:
+            # perform down sampling using stride of 2, according to [1].
+            self.res_conv = Conv2D(
+                self.__channels, strides=2, kernel_size=(1, 1), kernel_initializer=INIT_SCHEME, padding="same", kernel_constraint=Binarize(), bias_constraint=Binarize())
+            self.res_bn = BatchNormalization(beta_constraint=Binarize(), gamma_constraint=Binarize())
+
+    def call(self, inputs):
+        res = inputs
+
+        x = self.conv_1(inputs)
+        x = self.bn_1(x)
+        x = tf.nn.relu(x)
+        x = self.conv_2(x)
+        x = self.bn_2(x)
+
+        if self.__down_sample:
+            res = self.res_conv(res)
+            res = self.res_bn(res)
+
+        # if not perform down sample, then add a shortcut directly
+        x = self.merge([x, res])
+        out = tf.nn.relu(x)
+        return out
+
+class ResNet18(Model):
+
+    def __init__(self, num_classes, **kwargs):
+        """
+            num_classes: number of classes in specific classification task.
+        """
+        super().__init__(**kwargs)
+        self.conv_1 = Conv2D(64, (7, 7), strides=2,
+                             padding="same", kernel_initializer="he_normal", kernel_constraint=Binarize(), bias_constraint=Binarize())
+        self.init_bn = BatchNormalization(beta_constraint=Binarize(), gamma_constraint=Binarize())
+        self.pool_2 = MaxPool2D(pool_size=(2, 2), strides=2, padding="same")
+        self.res_1_1 = ResnetBlock(64)
+        self.res_1_2 = ResnetBlock(64)
+        self.res_2_1 = ResnetBlock(128, down_sample=True)
+        self.res_2_2 = ResnetBlock(128)
+        self.res_3_1 = ResnetBlock(256, down_sample=True)
+        self.res_3_2 = ResnetBlock(256)
+        self.res_4_1 = ResnetBlock(512, down_sample=True)
+        self.res_4_2 = ResnetBlock(512)
+        self.avg_pool = GlobalAveragePooling2D()
+        self.flat = Flatten()
+        self.fc = Dense(num_classes, activation="softmax")
+
+    def call(self, inputs):
+        out = self.conv_1(inputs)
+        out = self.init_bn(out)
+        out = tf.nn.relu(out)
+        out = self.pool_2(out)
+        for res_block in [self.res_1_1, self.res_1_2, self.res_2_1, self.res_2_2, self.res_3_1, self.res_3_2, self.res_4_1, self.res_4_2]:
+            out = res_block(out)
+        out = self.avg_pool(out)
+        out = self.flat(out)
+        out = self.fc(out)
+        return out
+
+model = ResNet18(10)
+model.build(input_shape = (None,28,28,1))
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.01),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model.fit(ds_train,epochs=30,validation_data=ds_test, verbose=1)
+
+print(model.weights)
+
+print(model.evaluate(ds_test))
